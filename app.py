e31016fb27b7e673226bb5a972afe1f636ccc866
Colin Bieberstein <colin@bieberstein.ca>
2022-06-05 18:46:56 -0600

    Updated app.py to generate obsidian files for data catalog

---
+++ app.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+refs_df = pd.read_parquet("data/BI.refs.parquet")
+#b_df = pd.read_parquet("data/BI.table-col-details.parquet")
+#c_df = pd.read_parquet("data/BI.view-col-details.parquet")
+
